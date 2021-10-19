93324cc33443c626d48792a5b0a422ddbe80bf9f
egemen6k <egemen.altikardesler@gmail.com>
2021-10-19 19:33:09 +0300

    +UI

--- BoxGame/Assets/Scripts/GameManager.cs
+++ BoxGame/Assets/Scripts/GameManager.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1f;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    private int _score;
+    public bool isGameActive;
 
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
+        _score = 0;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -21,11 +29,23 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        _score += scoreToAdd;
+        scoreText.text = "Score: " + _score;
     }
 }
--- BoxGame/Assets/Scripts/Target.cs
+++ BoxGame/Assets/Scripts/Target.cs
@@ -7,13 +7,17 @@ public class Target : MonoBehaviour
     private float _minSpeed = 12;
     private float _maxSpeed = 16;
     private float _maxTorque = 10;
-    private float xRange = 4;
-    private float ySpawnPos = -2;
+    private float _xRange = 4;
+    private float _ySpawnPos = -2;
     private Rigidbody _rb;
+    private GameManager _gm;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
     {
+        _gm = GameObject.Find("Game Manager").GetComponent<GameManager>();
         _rb = GetComponent<Rigidbody>();
         _rb.AddForce(RandomForce(), ForceMode.Impulse);
         _rb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(),ForceMode.Impulse);;
@@ -28,11 +32,17 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
+        _gm.UpdateScore(pointValue);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
         Destroy(gameObject);
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (!other.CompareTag("Bad"))
+        {
+            _gm.GameOver();
+        }
         Destroy(gameObject);
     }
 
@@ -48,6 +58,6 @@ public class Target : MonoBehaviour
 
     Vector3 RandomSpawnPos()
     {
-        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos, 0);
+        return new Vector3(Random.Range(-_xRange, _xRange), _ySpawnPos, 0);
     }
 }
